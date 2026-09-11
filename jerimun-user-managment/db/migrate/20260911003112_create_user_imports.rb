5a30b105b5328e00337886cb46a78018ae5869ca
Fernando Rodrigues <dombalder@gmail.com>
2026-09-10 22:08:54 -0300

    feat(jerimun): base database models

--- jerimun-user-managment/db/migrate/20260911003112_create_user_imports.rb
+++ jerimun-user-managment/db/migrate/20260911003112_create_user_imports.rb
@@ -1,13 +1,13 @@
 class CreateUserImports < ActiveRecord::Migration[8.1]
   def change
     create_table :user_imports do |t|
-      t.string :status
-      t.integer :total_rows
-      t.integer :processed_rows
-      t.integer :successful_rows
-      t.integer :failed_rows
+      t.string :status,            null: false, default: "pending"
+      t.integer :total_rows,       null: false, default: 0
+      t.integer :processed_rows,   null: false, default: 0
+      t.integer :successful_rows,  null: false, default: 0
+      t.integer :failed_rows,      null: false, default: 0
       t.text :error_message
-      t.references :created_by, null: false, foreign_key: true
+      t.references :created_by, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
